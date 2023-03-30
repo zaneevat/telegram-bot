dc355bc440f477c694f53db701f1fa9eb733390a
Maxim Semkin <semkin.maksim@mail.ru>
2023-03-30 21:41:55 +0300

    Add keyboard

--- internal/app/commands/commander.go
+++ internal/app/commands/commander.go
@@ -1,6 +1,8 @@
 package commands
 
 import (
+	"encoding/json"
+	"fmt"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/zaneevat/telegram-bot/internal/service/product"
 	"log"
@@ -9,6 +11,10 @@ import (
 type Commander struct {
 	bot            *tgbotapi.BotAPI
 	productService *product.Service
+}
+
+type CommandData struct {
+	Offset int `json:"offset"`
 }
 
 func NewCommander(bot *tgbotapi.BotAPI, productService *product.Service) *Commander {
@@ -24,6 +30,18 @@ func (c *Commander) HandleUpdate(update tgbotapi.Update) {
 			log.Printf("recovered from panic: %v", panicValue)
 		}
 	}()
+
+	if update.CallbackQuery != nil {
+		parsedData := CommandData{}
+		json.Unmarshal([]byte(update.CallbackQuery.Data), &parsedData)
+		msg := tgbotapi.NewMessage(
+			update.CallbackQuery.Message.Chat.ID,
+			fmt.Sprintf("Parsed: %+v\n", parsedData),
+		)
+
+		c.bot.Send(msg)
+		return
+	}
 
 	if update.Message == nil {
 		return
--- internal/app/commands/list.go
+++ internal/app/commands/list.go
@@ -1,6 +1,9 @@
 package commands
 
-import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+import (
+	"encoding/json"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
 
 func (c *Commander) List(inputMessage *tgbotapi.Message) {
 	outputMsg := "Here all the products: \n\n"
@@ -12,6 +15,16 @@ func (c *Commander) List(inputMessage *tgbotapi.Message) {
 	}
 
 	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, outputMsg)
+
+	serializedData, _ := json.Marshal(CommandData{
+		Offset: 21,
+	})
+
+	msg.ReplyMarkup = tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("Next page", string(serializedData)),
+		),
+	)
 
 	c.bot.Send(msg)
 }
